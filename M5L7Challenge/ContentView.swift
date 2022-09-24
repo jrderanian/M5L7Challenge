25abad43265d31de2f560935cf53bcff7922fe07
John Deranian <fun@scooby.cc>
2022-09-24 15:22:51 -0500

    Initial Commit

---
+++ M5L7Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M5L7Challenge
+//
+//  Created by John Deranian on 9/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5L7Challenge/M5L7ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L7ChallengeApp.swift
+//  M5L7Challenge
+//
+//  Created by John Deranian on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct M5L7ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
